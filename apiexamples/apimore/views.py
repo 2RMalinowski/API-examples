82e70fd855e47954fc0733fb3e2b4f664120250c
malin4 <2rmalinowski@gmail.com>
2018-06-19 12:59:26 +0200

    casching marker added

--- apiexamples/apimore/views.py
+++ apiexamples/apimore/views.py
@@ -1,12 +1,19 @@
 import requests
 from django.conf import settings
 from django.shortcuts import render
+from .forms import DictionaryForm
 
 
 def geo(request):
-    ip_address = request.META.get('HTTP_X_FORWARDED_FOR', '')
-    response = requests.get('http://freegeoip.net/json/%s' % ip_address)
-    geodata = response.json()
+    is_cached = ('geodata' in request.session)
+
+    if not is_cached:
+        ip_address = request.META.get('HTTP_X_FORWARDED_FOR', '')
+        response = requests.get('http://freegeoip.net/json/%s' % ip_address)
+        request.session['geodata'] = response.json()
+
+    geodata = request.session['geodata']
+
     return render(request, 'apimore/geo.html', {
         'ip': geodata['ip'],
         'country': geodata['country_name'],
@@ -15,7 +22,16 @@ def geo(request):
         'latitude': geodata['latitude'],
         'longitude': geodata['longitude'],
         'api_key': settings.API_KEY,
+        'is_cached': is_cached
     })
 
 
-
+def oxford(request):
+    search_result = {}
+    if 'word' in requests.GET:
+        form = DictionaryForm(request.GET)
+        if form.is_valid():
+            search_result = form.search()
+    else:
+        form = DictionaryForm()
+    return render(request, 'core/oxford.html', {'form': form, 'search_result': search_result})
